39b167105d347bb7cd7c29acee49fc6a6b19d633
Recep CAN <recepcn61@gmail.com>
2024-07-26 18:58:46 +0300

    connect to the databse and add dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
- 
+ import mongoose from 'mongoose'
+ import dotenv from 'dotenv'
 const app = express()
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_URI).then(
+    ()=>{console.log("mongodb is connected")}
+).catch((e)=>{
+    console.log(e)
+})
 
 app.listen(5000,()=>{
     console.log("server is running on port  5000")
