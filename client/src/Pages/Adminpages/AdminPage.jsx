c05545d5bec665de33a3372773d54edac39ca05e
Recep CAN <recepcn61@gmail.com>
2024-07-27 01:04:03 +0300

    admin page sidebar

--- client/src/Pages/Adminpages/AdminPage.jsx
+++ client/src/Pages/Adminpages/AdminPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function AdminPage() {
   return (
-    <div className='min-h-screen bg-gray-100 flex items-center justify-center '>AdminPage</div>
+    <div className='min-h-screen relative bg-gray-100 flex items-center justify-center '>
+    
+    <div className='w-1/4 h-[750px] absolute  left-0 top-20 rounded-lg  p-10 bg-gray-200'>
+    <h1 className='text-3xl text-red-800 font-bold'>Pages </h1>
+    </div>
+    </div>
   )
 }
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -67,28 +67,3 @@ export const signin = async (req, res, next) => {
       next(error);
     }
   };
-  // export const signin = async (req, res, next) => {
-//     const { email, password } = req.body
-
-
-//     if (!email || !password || email === '' || password === '') {
-//         next(errorHandler(400, "all fields are required"))
-//     }
-
-//     try {
-//         const validUser = await User.findOne({ email })
-//         if (!validUser) {
-//             next(errorHandler(404, "user not found "))
-           
-//      const validPassword = bcrypt.compareSync(password, validUser.password)
-//             if (!validPassword) {
-//                 return next(errorHandler(400, "invalid password"))
-//             }
-
-//             const token = jwt.sign( { id: validUser._id }, process.env.JWT_SECRET)
-//             res.status(200).cookie('access_token', token, { httpOnly: true }).json( validUser)
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// }
