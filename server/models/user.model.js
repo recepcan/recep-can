12ade070cc802d8747fce2b0b63700c74fa9550a
Recep CAN <recepcn61@gmail.com>
2024-07-26 19:31:35 +0300

    create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+// Kullanıcı şeması oluşturma
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  }
+},{timestamps:true});
+
+
+
+// Kullanıcı modelini oluşturma
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
