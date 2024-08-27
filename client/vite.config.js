3b98a4854f8912e6854359ee18b85f07c737fc71
Recep CAN <recepcn61@gmail.com>
2024-08-28 00:48:49 +0300

    change link from pj and viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://my-personal-blog-recepcan.onrender.com',
         changeOrigin: true, // Origin başlığını değiştirir
         secure: false, // HTTPS yerine HTTP kullanıyorsanız
       },
