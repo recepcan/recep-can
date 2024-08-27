3054b99540339f3bc40cffac559c5a33a4594021
Recep CAN <recepcn61@gmail.com>
2024-08-27 17:55:14 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.use(express.static(path.join(__dirname,"/client")));
+app.use(express.static(path.join(__dirname,"/client/build")));
 
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'/client/build','index.html'))
