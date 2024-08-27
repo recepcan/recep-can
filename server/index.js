f41705c1fab508d270211bba0b7707c065f55e2f
Recep CAN <recepcn61@gmail.com>
2024-08-28 00:44:23 +0300

    - dist

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use('/api/contact',contactRoutes)
 
 app.use(express.static(path.join(__dirname,'/client/dist')))
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+    res.sendFile(path.join(__dirname,'client','index.html'))
 })
 
 
