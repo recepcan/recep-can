908c9408bf55477017ada547f27518248fb3997a
Recep CAN <recepcn61@gmail.com>
2024-09-02 22:59:07 +0300

    footer revize

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
     <div className='w-full   bg-white bottom-0 shadow-inner shadow-gray-400 dark:border-t-2 p-5
-         border-blue-500 rounded-t-3xl text-black flex flex-col   lg:flex-row justify-center text-center items-center
+         border-blue-500 rounded-t-3xl text-black flex flex-col  max-lg:space-y-8 lg:flex-row justify-center text-center items-center
         dark:text-white dark:bg-black transition-colors duration-300 dark:z-50 flex-1'>
       <div className='lg:w-1/3 w-full space-y-5'>
         <Link to="/">
@@ -15,67 +15,68 @@ function Footer() {
           </div>
         </Link>
         <h2> Copyright©2024 Recep Can. All rights reserved.</h2>
+        <hr className='lg:hidden  border-black  dark:border-sky-400 '/>
       </div>
 
       <div className='  max-lg:w-full flex-wrap flex flex-col items-center justify-center 
               dark:text-white text-lg border-white'>
         <h1 className='text-2xl font-bold font-inter'>Bilgi</h1>
         <div className='dark:text-gray-400'>
-          <button className=' flex items-center justify-center  text-lg p-2   w-[300px]     rounded-lg  hover:scale-110 transition-all '>
+          <button className=' flex items-center justify-center  text-lg p-2   w-[300px] hover:text-white     rounded-lg  hover:scale-110 transition-all '>
             Site Tasarlama
           </button>
 
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]    rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px] hover:text-white    rounded-lg  hover:scale-110 transition-all'>
             Site Yayınlama
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]     rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px]  hover:text-white    rounded-lg  hover:scale-110 transition-all'>
             Alan Adı Transferi
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]     rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px]  hover:text-white    rounded-lg  hover:scale-110 transition-all'>
             Güvenlik
           </button>
         </div>
-
+       
       </div>
 
       <div className='  max-lg:w-full flex-wrap flex flex-col items-center justify-center 
           dark:text-white text-lg border-white'>
         <h1 className='text-2xl font-bold font-inter'>Hizmetler</h1>
         <div className='dark:text-gray-400'>
-          <button className=' flex items-center justify-center  text-lg p-2   w-[300px]     rounded-lg  hover:scale-110 transition-all '>
+          <button className=' flex items-center justify-center  text-lg p-2   w-[300px]  hover:text-white    rounded-lg  hover:scale-110 transition-all '>
             Web Tasarım
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]     rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px]    hover:text-white  rounded-lg  hover:scale-110 transition-all'>
             Web Programlama
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]    rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px]  hover:text-white   rounded-lg  hover:scale-110 transition-all'>
             Hosting
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]     rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px]  hover:text-white    rounded-lg  hover:scale-110 transition-all'>
             Domain-Alan Adı
           </button>
         </div>
-
+        
       </div>
 
       <div className='  max-lg:w-full flex-wrap flex flex-col items-center justify-center 
       dark:text-white text-lg border-white'>
         <h1 className='text-2xl font-bold font-inter'>Sosyal Medya</h1>
         <div className='dark:text-gray-400'>
-          <button className=' flex items-center justify-center  text-lg p-2   w-[300px]     rounded-lg  hover:scale-110 transition-all '>
+          <button className=' flex items-center justify-center  text-lg p-2 hover:text-white  w-[300px]     rounded-lg  hover:scale-110 transition-all '>
             İnstagram <FaInstagram className='text-xl text-red-400' />
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]     rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px]  hover:text-white    rounded-lg  hover:scale-110 transition-all'>
             Linked <FaLinkedin className='text-xl text-sky-400' />
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]    rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px] hover:text-white    rounded-lg  hover:scale-110 transition-all'>
             Github <FaGithub className='text-xl text-purple-500' />
           </button>
-          <button className='flex items-center justify-center  text-lg p-2  w-[300px]     rounded-lg  hover:scale-110 transition-all'>
+          <button className='flex items-center justify-center  text-lg p-2  w-[300px]  hover:text-white    rounded-lg  hover:scale-110 transition-all'>
             Whatsapp <FaWhatsapp className='text-xl text-green-400' />
           </button>
         </div>
-
+        
       </div>
 
     </div>
