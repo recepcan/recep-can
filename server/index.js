2324bd7abbdf144ac2f455cfb61cfcb75b681d1a
Recep CAN <recepcn61@gmail.com>
2024-09-07 20:13:39 +0300

    no cors

--- server/index.js
+++ server/index.js
@@ -16,11 +16,7 @@ app.use(express.json())
 
 
 
-app.use(cors({
-    origin: ["https://my-personal-blog-onvercel.vercel.app/"],
-    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
-    credentials: true
-}))
+app.use(cors())
 
 
 dotenv.config()
