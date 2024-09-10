88002f5ad7f063b5dadbcd4bec087c0a34d7e166
Recep CAN <recepcn61@gmail.com>
2024-09-10 14:35:39 +0300

    verifytoken err

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/create', [verifyToken,verifyAdmin], create)
 router.get('/getposts', getposts)
-router.delete('/deletepost/:postId/:userId', [verifyToken,verifyAdmin], deletepost)
+router.delete('/deletepost/:postId/:userId', [verifyAdmin], deletepost)
 router.patch('/updatepost/:postId/:userId', [verifyToken,verifyAdmin], updatepost)
 
 
--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
     
-    return  next(errorHandler(401, 'Unauthorized'));
+    return  next(errorHandler(401, 'Unauthorized-Token Bulunamadı'));
    
     
     
